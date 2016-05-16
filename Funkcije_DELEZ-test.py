43ce9fff0d64d41d4a3dccff91c4bbcac9ba3f08
tnt9 <nina.tratnik@gmail.com>
2016-05-16 10:29:39 +0200

    Add files via upload

--- Funkcije_DELEZ-test.py
+++ Funkcije_DELEZ-test.py
@@ -1,7 +1,7 @@
 from Funkcije_DELEZ import percentage
 
 def testing_function_percentage():
-    assert percentage(30.0, 120) == 25.0
+    assert percentage(30, 120) == 25.0
     return "testing_function_percentage() passed successfully"
 
 print testing_function_percentage()
